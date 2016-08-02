1e4567a5cb058f9e3536ca59b099e2f10f344318
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-08-02 18:41:29 +0200

    Set up http request mocking

--- test/index.js
+++ test/index.js
@@ -1,6 +1,27 @@
 var FlightStats = require( '..' )
-var assert = require( 'assert' )
+
+global.client = null
+global.scope = null
 
 suite( 'FlightStats', function() {
+
+  suiteSetup( 'init client', function() {
+
+    client = new FlightStats({
+      appId: process.env['FLIGHTSTATS_APP_ID'] || 'xxxxxxxxxx',
+      apiKey: process.env['FLIGHTSTATS_API_KEY'] || 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+    })
+
+    assert.ok( client )
+    assert.ok( client instanceof FlightStats )
+
+  })
+
+  suiteSetup( 'init http', function() {
+    var nock = require( 'nock' )
+    scope = nock( client.options.baseUrl )
+  })
+
+  require( './airlines' )
 
 })
